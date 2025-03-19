698c0f6b070b8ff550dec64605ab169af670a82a
AJ Moon <fm563017@gmail.com>
2025-03-19 15:58:12 +0900

    static variable

--- Basic_C_Language/ch13/static.c
+++ Basic_C_Language/ch13/static.c
@@ -2,7 +2,14 @@
 
 void static_function(int n);
 
+// if the global variable is static, it means it cannot be accessed in other
+// files.
+static int hap = 0;
+
 int main(int argc, char *argv[]) {
+  // it is in the register. it can be loaded quicker. register > memory (spped
+  // wise)
+  register int i;
   for (int n = 1; n <= 10; n++) {
     static_function(n);
   }
@@ -10,7 +17,6 @@ int main(int argc, char *argv[]) {
 }
 
 void static_function(int n) {
-  static int hap = 0;
   hap += n;
   printf("hap: %d\n", hap);
 }
