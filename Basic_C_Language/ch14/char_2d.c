07d93ffc35d7b03cd4c422511fd97c5268a38ca9
AJ Moon <fm563017@gmail.com>
2025-03-19 16:51:35 +0900

    additional codes added

--- Basic_C_Language/ch14/char_2d.c
+++ Basic_C_Language/ch14/char_2d.c
@@ -8,15 +8,24 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < count; i++) {
     fgets(names[i], 20, stdin);
+    // get rid of 'enter'
     names[i][strlen(names[i]) - 1] = '\0';
   }
   printf("\n");
 
   for (int i = 0; i < count; i++) {
-    for (int j = 0; j < strlen(names[i]); j++) {
-      printf("%c", names[i][j]);
-    }
+    printf("%s", names[i]);
     printf("\n");
+  }
+
+  char s_name[20];
+  printf("search name? ");
+  fgets(s_name, 20, stdin);
+  s_name[strlen(s_name) - 1] = '\0';
+
+  for (int i = 0; i < count; i++) {
+    if (strcmp(s_name, names[i]) == 0)
+      printf("Name found on %d\n", i + 1);
   }
 
   return 0;
