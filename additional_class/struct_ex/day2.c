af9d02134e701a9c6672e39e651fccbc3de9a909
AJ Moon <fm563017@gmail.com>
2025-03-20 23:51:13 +0900

    struct example completed

--- additional_class/struct_ex/day2.c
+++ additional_class/struct_ex/day2.c
@@ -34,20 +34,20 @@ void print_scores(Student *student, const int size) {
   printf("\n");
 }
 
-Student *load_data(Student *student, int *size) {
+Student *load_data(int *size) {
   FILE *fptr = fopen("student.txt", "r");
-  int temp_size = 0;
-  Student *temp;
+  Student *temp = NULL;
 
   if (fptr == NULL) {
-    printf("파일이 존재하지 않았습니다.\n");
+    printf("파일이 존재하지 않습니다.\n");
   } else {
     printf("학생 정보가 로드되었습니다.\n");
     fscanf(fptr, "%d", size);
-    temp = (Student *)malloc(sizeof(Student) * (*size - 1));
+    temp = (Student *)malloc(sizeof(Student) * (*size));
     for (int i = 0; i < *size; i++) {
       fscanf(fptr, "%s %d", temp[i].name, &temp[i].score);
     }
+    (*size)++;
   }
   fclose(fptr);
   return temp;
@@ -57,10 +57,12 @@ int main(int argc, char *argv[]) {
   int size = 1, menu_input, data_write = 0;
   double avg;
   Student *students = (Student *)malloc(sizeof(Student));
+  Student *temp = load_data(&size);
   FILE *fptr;
+  if (temp != NULL)
+    students = temp;
 
   do {
-    students = load_data(students, &size);
     printf("---- 메뉴 ----\n");
     printf("1. 학생 추가\n");
     printf("2. 학생 정보 출력\n");
@@ -73,14 +75,15 @@ int main(int argc, char *argv[]) {
     case 1:
       if (size == 1) {
         input_scores(students, size);
-        size++;
-        data_write = 1;
+        ++size;
       } else {
         students = realloc(students, sizeof(Student) * size);
         input_scores(students, size);
-        size++;
+        ++size;
       }
+      data_write = 1;
       break;
+
     case 2:
       printf("학생 목록:\n");
       if (size == 1)
@@ -89,30 +92,37 @@ int main(int argc, char *argv[]) {
         print_scores(students, size);
       }
       break;
+
     case 3:
       avg = calc_avg(students, size);
       printf("평균 점수: %.2lf\n", avg);
       break;
-    case 4:
-      if (data_write == 1) {
-        fptr = fopen("student.txt", "a");
-        // checking if the file is created
-        if (fptr == NULL) {
-          printf("파일이 로드되지 않았습니다.\n");
-          exit(0);
-        }
-        fprintf(fptr, "%d\n", size - 1);
-        for (int i = 0; i < size - 1; i++) {
-          fprintf(fptr, "%s %d\n", students[i].name, students[i].score);
-        }
-        fclose(fptr);
 
-        printf("학생 정보가 저장되었습니다.\n");
-        printf("프로그램을 종료합니다.\n");
-      } else {
+    case 4:
+      if (data_write == 0) {
         printf("저장 할 데이터가 존재하지 않습니다.\n");
         printf("프로그램을 종료합니다.\n");
+        exit(0);
       }
+
+      fptr = fopen("student.txt", "w");
+      // checking if the file is created
+      if (fptr == NULL) {
+        printf("파일이 로드되지 않았습니다.\n");
+        exit(0);
+      }
+      fprintf(fptr, "%d\n", size - 1);
+      for (int i = 0; i < size - 1; i++) {
+        fprintf(fptr, "%s %d\n", students[i].name, students[i].score);
+      }
+      fclose(fptr);
+
+      printf("학생 정보가 저장되었습니다.\n");
+      printf("프로그램을 종료합니다.\n");
+      break;
+
+    default:
+      printf("올바른 옵션이 아닙니다. 다시 시도해주세요.\n");
       break;
     }
   } while (menu_input != 4);
