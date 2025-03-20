4d5ad3e5c51513ce72ffec1c31f90607562e977f
AJ Moon <fm563017@gmail.com>
2025-03-20 11:28:48 +0900

    more examples

--- Basic_C_Language/ch15/double_pointer.c
+++ Basic_C_Language/ch15/double_pointer.c
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void sawp_ptr(char **ppa, char **ppb);
+void print_ary(int (*)[4]);
 
 int main(int argc, char *argv[]) {
+  // pointer to pointer example
   /*int n1 = 100, n2 = 200;*/
   /*int *p = &n1;*/
   /*int **dp = &p;*/
@@ -17,12 +18,18 @@ int main(int argc, char *argv[]) {
   /*dp++;*/
   /*printf("&n1: %p, p: %p, &p: %p, dp: %p\n", &n1, p, &p, dp);*/
 
-  char *pa = "success";
-  char *pb = "failure";
+  // swap addr
+  /*char *pa = "success";*/
+  /*char *pb = "failure";*/
+  /**/
+  /*printf("pa-> %s, pb-> %s\n", pa, pb);*/
+  /*sawp_ptr(&pa, &pb);*/
+  /*printf("pa-> %s, pb-> %s\n", pa, pb);*/
 
-  printf("pa-> %s, pb-> %s\n", pa, pb);
-  sawp_ptr(&pa, &pb);
-  printf("pa-> %s, pb-> %s\n", pa, pb);
+  // pointer to 2d array
+  int ary[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+  print_ary(ary);
 
   return 0;
 }
@@ -32,4 +39,13 @@ void sawp_ptr(char **ppa, char **ppb) {
   pt = *ppa;
   *ppa = *ppb;
   *ppb = pt;
+}
+
+void print_ary(int (*pa)[4]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      printf("%5d", pa[i][j]);
+    }
+    printf("\n");
+  }
 }
