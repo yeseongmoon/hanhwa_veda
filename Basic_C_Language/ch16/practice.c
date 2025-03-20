34db811987c666d08c5f6cdc3b22ae59b5ecf0dc
AJ Moon <fm563017@gmail.com>
2025-03-20 16:44:38 +0900

    practice example of malloc and realloc

---
+++ Basic_C_Language/ch16/practice.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int size = 5, input, count = 0;
+  int *ptr = (int *)malloc(sizeof(int) * size);
+  while (1) {
+    printf("Enter a number(-1 for exit) : ");
+    scanf("%d", &input);
+    if (input == -1)
+      break;
+    if (count == size) {
+      size += 10;
+      ptr = (int *)realloc(ptr, sizeof(int) * size);
+    }
+    ptr[count] = input;
+    count++;
+  }
+  for (int i = 0; i < size; i++) {
+    printf("%d ", ptr[i]);
+  }
+  printf("\n");
+  return 0;
+}
