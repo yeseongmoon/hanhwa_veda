229d41fceacdb4e81867e53c517b92ab12d7e8e1
AJ Moon <fm563017@gmail.com>
2025-03-23 22:30:58 +0900

    slight changes for efficiency

--- Basic_C_Language/ch17/menu.c
+++ Basic_C_Language/ch17/menu.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]) {
     }
   } while (choice != 5);
 
-  // free memory
+  // cleanup the memory
   food *tmp;
   while (head != NULL) {
     tmp = head;
@@ -75,6 +75,7 @@ void input_menu(food **first, food **end) {
   food *tmp = NULL;
   char input_name[20];
   float food_price;
+
   while (1) {
     printf("Please enter a food name(x to exit): ");
     scanf("%s", input_name);
@@ -100,94 +101,107 @@ void input_menu(food **first, food **end) {
 
 void output_menu(food *list) {
   // if the head is NULL, stop the printing out process.
-  food *tmp = list;
-  if (tmp == NULL) {
+  if (list == NULL)
     printf("The list is empty.\n");
-  }
-  while (tmp != NULL) {
-    printf("Name: %s, price: %.2f\n", tmp->food_name, tmp->price);
-    tmp = tmp->next;
+
+  else {
+    food *tmp = list;
+    while (tmp != NULL) {
+      printf("Name: %s, price: %.2f\n", tmp->food_name, tmp->price);
+      tmp = tmp->next;
+    }
   }
 }
 
 void search_menu(food *list) {
-  char input_name[30];
-  int idx, found;
-  food *tmp = NULL;
-  while (1) {
-    tmp = list;
+  // if the head is NULL, stop the searching process.
+  if (list == NULL)
+    printf("The list is empty.\n");
 
-    // if the head is NULL, stop the searching process.
-    if (tmp == NULL) {
-      printf("The list is empty.\n");
-      break;
-    }
-    found = idx = 0;
-    printf("What food are you looking for?(x to exit) ");
-    scanf("%s", input_name);
-    if (strcmp(input_name, "x") == 0) {
-      printf("Finished searching\n");
-      break;
-    }
-    while (tmp != NULL) {
-      if (strcmp(tmp->food_name, input_name) == 0) {
-        printf("Food is found! #%d on the list\n", idx + 1);
-        printf("Its price: %.2f\n", tmp->price);
-        found = 1;
+  else {
+    char input_name[20];
+    int idx, found;
+    food *tmp = NULL;
+
+    while (1) {
+      tmp = list;
+      found = idx = 0;
+      printf("What food are you looking for?(x to exit) ");
+      scanf("%s", input_name);
+      if (strcmp(input_name, "x") == 0) {
+        printf("Finished searching\n");
+        break;
       }
-      idx++;
-      tmp = tmp->next;
-    }
-    if (found == 0) {
-      printf("NUFood not found on the list.\n");
+      while (tmp != NULL) {
+        if (strcmp(tmp->food_name, input_name) == 0) {
+          printf("Food is found! #%d on the list\n", idx + 1);
+          printf("Its price: %.2f\n", tmp->price);
+          found = 1;
+        }
+        idx++;
+        tmp = tmp->next;
+      }
+      if (found == 0) {
+        printf("Food not found.\n");
+      }
     }
   }
 }
 
 void remove_menu(food **first, food **end) {
-  food *prev = NULL, *tmp = NULL;
-  char input_name[20];
-  while (1) {
-    prev = tmp = *first;
+  if (*first == NULL)
+    printf("The list is empty.\n");
 
-    // if the head is NULL, stop the removing process.
-    if (*first == NULL) {
-      printf("The list is empty.\n");
-      break;
-    }
-
-    printf("Enter the food name you would like to delete:(x to exit) ");
-    scanf("%s", input_name);
-    if (strcmp(input_name, "x") == 0) {
-      printf("Finish removing\n");
-      break;
-    }
-    while (tmp != NULL) {
-      if (strcmp(tmp->food_name, input_name) == 0) {
+  else {
+    food *prev = NULL, *curr = NULL;
+    char input_name[20];
+    while (1) {
+      // if the head is NULL, stop the removing process.
+      if (*first == NULL) {
+        printf("The list is empty.\n");
         break;
       }
-      prev = tmp;
-      tmp = prev->next;
-    }
 
-    if (*first == tmp) {
-      printf("%s is removed from the list.\n", tmp->food_name);
-      *first = tmp->next;
-    }
+      prev = curr = *first;
 
-    else if (tmp == *end) {
-      *end = prev;
-      printf("%s is removed from the list.\n", tmp->food_name);
-      prev->next = NULL;
-    }
+      printf("Enter the food name you would like to delete:(x to exit) ");
+      scanf("%s", input_name);
+      if (strcmp(input_name, "x") == 0) {
+        printf("Finish removing\n");
+        break;
+      }
 
-    else if (tmp != NULL) {
-      printf("%s is removed from the list.\n", tmp->food_name);
-      prev->next = tmp->next;
-    }
+      while (curr != NULL) {
+        if (strcmp(curr->food_name, input_name) == 0) {
+          break;
+        }
+        prev = curr;
+        curr = prev->next;
+      }
 
-    else {
-      printf("The food name is not existed on the list.\n");
+      if (*first == curr) {
+        printf("%s is removed from the list.\n", curr->food_name);
+        *first = curr->next;
+      }
+
+      else if (curr == *end) {
+        *end = prev;
+        printf("%s is removed from the list.\n", curr->food_name);
+        prev->next = NULL;
+      }
+
+      else if (curr != NULL) {
+        printf("%s is removed from the list.\n", curr->food_name);
+        prev->next = curr->next;
+      }
+
+      else {
+        printf("The food name is not existed on the list.\n");
+      }
+
+      // immediate freeing memory to prevent from potential memory leak &
+      // wasting memory
+      free(curr);
     }
   }
 }
