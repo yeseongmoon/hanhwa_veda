3cffe6a9495b7f7489ca77f1819d99fc3aeea23e
AJ Moon <fm563017@gmail.com>
2025-03-26 17:24:29 +0900

    reference example

---
+++ Basic_Cpp_language/ch03/reference.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+int main(int argc, char *argv[]) {
+  int n = 10;
+  // reference can only be assigned with variables,
+  // but if it's const, it can be e.g. const int &r = 50;
+  // int &r = n;
+  int m = 50;
+  // r = m;
+
+  // cout << "r: " << &r << " " << r << endl;
+  // cout << "n: " << &n << " " << n << endl;
+  // cout << "m: " << &m << " " << m << endl;
+  cout << "n: " << n << endl;
+  cout << "m: " << m << endl;
+
+  swap(m, n);
+
+  cout << "n: " << n << endl;
+  cout << "m: " << m << endl;
+
+  return 0;
+}
