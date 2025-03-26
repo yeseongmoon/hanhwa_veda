8825822bc2a409f33ab3562212242051ee659cee
AJ Moon <fm563017@gmail.com>
2025-03-26 10:58:19 +0900

    first cpp example

---
+++ Basic_Cpp_language/ch02/input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  int i, j;
+
+  std::cout << "Enter num_1: ";
+  std::cin >> i;
+
+  std::cout << "Enter num_2: ";
+  std::cin >> j;
+
+  std::cout << "num_1 + num_2 = " << i + j << std::endl;
+
+  return 0;
+}
