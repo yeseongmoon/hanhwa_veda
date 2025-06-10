231cb0a25fbcd1962b156973b38edb584f7c8b53
AJ <fm563017@gmail.com>
2025-06-10 15:50:45 +0900

    assembly code

--- Embedded_system/arm/practice.s
+++ Embedded_system/arm/practice.s
@@ -1,34 +1,68 @@
+	.arch armv7-a
+	.fpu vfpv3-d16
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"practice.c"
   .data
-statement: .asscii "Multiple of 3\n"
-num: .word 0
-  .text
-  .global	main
-
+message: .asciz "Multiple of 3\n"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"%d\012\000"
+	.text
+	.align	1
+	.global	main
+	.syntax unified
+	.thumb
+	.thumb_func
+	.type	main, %function
 main:
-  @ args = 0, pretend = 0, frame = 16
-  @ frame_needed = 1, uses_anonymous_args = 0
-  sub  sp, sp, #16
-  add  r7, sp, #0
-  movs r3, #0
-  str r3, [r7, #4]
-  b .Loop
-.L1:
-  ldr r3, [r7, #4]
-  cmp r3, #100
-  bgt .Loop
-
-  ldr r3, =statement
-  mov r0, r3
-  bl printf(PLT)
-
-  ldr r3, [r7, #4]
-  adds r3, r3, #1
-  str r3, [r7, #4]
-
-  b .Loop
-.Loop:
-  ldr r3, [r7, #4]
-  cmp r3, #100
-  ble .L1
-
-  .size main, .-main
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{r7, lr}
+	sub	sp, sp, #8
+	add	r7, sp, #0
+	movs	r3, #0
+	str	r3, [r7, #4]
+	b	.L2
+.L4:
+  ldr r2, [r7, #4]
+  movs r2, r2 mod 3
+  cmp r2, #0
+  beq .L6
+.L3:
+	ldr	r1, [r7, #4]
+	ldr	r3, .L5
+.LPIC0:
+	add	r3, pc
+	mov	r0, r3
+	bl	printf(PLT)
+	ldr	r3, [r7, #4]
+	adds	r3, r3, #1
+	str	r3, [r7, #4]
+.L2:
+	ldr	r3, [r7, #4]
+	cmp	r3, #100
+	ble	.L4
+	movs	r3, #0
+	mov	r0, r3
+	adds	r7, r7, #8
+	mov	sp, r7
+	@ sp needed
+	pop	{r7, pc}
+.L6:
+	.align	2
+.L5:
+	.word	.LC0-(.LPIC0+4)
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",%progbits
